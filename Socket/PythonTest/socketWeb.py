680226144b6a98ea0bd5bbf9442775445265c580
Yaba <henrik.n@live.se>
2017-05-03 23:49:05 +0200

    Nya uppgiften

--- Socket/PythonTest/socketWeb.py
+++ Socket/PythonTest/socketWeb.py
@@ -1,25 +1,23 @@
 from socket import *
 
+print("Creating socket..")
 conn = socket(AF_INET, SOCK_STREAM)
-
-print("Socket created")
 
 adress = "ingonline.nu"
 port = 80
 
+print("Connecting socket to " + adress + " with port " + str(port) + "..")
 conn.connect((adress,port))
-
-print("Socket connected to " + adress + " with port " + str(port) )
 
 h_message  = "GET /tictactoe/index.php?board=eeeeeeeee HTTP/1.1\r\n"
 h_message += "Host: " + adress + "\r\n"
 h_message += "Connection: close\r\n\r\n"
 
+print("Sending message..")
 conn.send(h_message.encode())
 
-print("Message sent successfully\r\n")
- 
+print("Receiving server response..")
+# Går att använda .decode() för att få ut responsen i bättre format
+s_reply = conn.recv(2048)
 
-s_reply = conn.recv(2048).decode()
-
-print("Server response\r\n\r\n" + s_reply)
+print("Server response\r\n\r\n" + str(s_reply))
