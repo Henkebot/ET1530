7eb8f51c75cd0bd61d92a8d995e4315c193d3f47
Henrik Nilsson <henrik.n@live.se>
2017-05-10 15:56:55 +0200

    Redo

--- Socket/Labb2/Version 2/UDPSender.py
+++ Socket/Labb2/Version 2/UDPSender.py
@@ -7,7 +7,7 @@ target_port         = 12000
 socket              = socket(AF_INET,SOCK_DGRAM)
 
 # Message Settings
-frequency           = 10
+frequency           = 50
 message_size        = 95
 message_sent        = 0
 
@@ -28,7 +28,7 @@ while (current_time < finished_time):
     unprocessed += (current_time - last_time) / sec_per_send
     last_time = current_time
 
-    while(unprocessed > 0):
+    while(unprocessed > 1):
         # Convert message to bytes
         message_sent += 1
         counter = (message_sent.to_bytes(8, byteorder='big')) 
